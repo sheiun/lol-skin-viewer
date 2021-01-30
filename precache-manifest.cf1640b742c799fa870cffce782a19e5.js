195eedb5468cfef0042e0424d2222c79c18ecd6f
sheiun <me.sheiun@gmail.com>
2021-01-30 22:14:22 +0800

    Updates

---
+++ precache-manifest.cf1640b742c799fa870cffce782a19e5.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04b5895b1dc546d9dec5",
+    "url": "/css/about.a5752eb0.css"
+  },
+  {
+    "revision": "615b8214394b8c9d5fdc",
+    "url": "/css/app.d3a3e44c.css"
+  },
+  {
+    "revision": "08e8598ac0e64e28d46f5b101f299fee",
+    "url": "/img/logo.08e8598a.svg"
+  },
+  {
+    "revision": "96877ceb88e41d59493963886abcd190",
+    "url": "/index.html"
+  },
+  {
+    "revision": "04b5895b1dc546d9dec5",
+    "url": "/js/about.5c3bd84f.js"
+  },
+  {
+    "revision": "615b8214394b8c9d5fdc",
+    "url": "/js/app.62937c71.js"
+  },
+  {
+    "revision": "a07283b712267b98bbaa",
+    "url": "/js/chunk-vendors.d5eb946c.js"
+  },
+  {
+    "revision": "0c23d48d9199f67ee99c266865738897",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
