d33a3ee9ba9574f314f6de8e86a1b7885f156cdb
sheiun <me.sheiun@gmail.com>
2021-01-31 06:38:47 +0800

    Delete debug line

--- src/script/spotlight.py
+++ src/script/spotlight.py
@@ -8,8 +8,6 @@ def get_video_id(query: str):
     BASE_URL = "https://www.youtube.com/c/SkinSpotlights"
     url = f"{BASE_URL}/search?query={encoded_search}"
     response = requests.get(url).text
-    with open("d.html", "w") as f:
-        f.write(response)
     term = '"videoId":"'
     start = response.find(term)
     vLen = response[start + len(term) :].find('"')
